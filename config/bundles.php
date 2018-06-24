3ab8364d7b7d1325b234443137a8cb5a23893f35
Deimantas Abelskis <ddeimantass@gmail.com>
2018-06-24 12:17:41 +0300

    alice fixtures and tests

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ tests/Functional/Controller/QuizControllerTest.php
@@ -0,0 +1,123 @@
+<?php
+
+
+namespace Tests\Functional\Controller;
+
+use App\Entity\Quiz;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use Nelmio\Alice\Loader\NativeLoader;
+
+class QuizControllerTest extends WebTestCase
+{
+    /**
+     * @var Client $client
+     */
+    private $client;
+    
+    /**
+     * @var EntityManager $em
+     */
+    private $em;
+    
+    public function setUp()
+    {
+        $this->client = self::createClient();
+        $kernel = self::bootKernel();
+    
+        $this->em = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        
+        $loader = new NativeLoader();
+        $loader->loadFile(__DIR__.'/../../DataFixture/QuizFixtures.yml');
+    }
+    
+    public function testQuizList()
+    {
+        $crawler = $this->client->request('GET', '/quiz/');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $crawler = $crawler->filter("table>tbody>tr");
+
+        $nodeValues = $crawler->each(
+            function (Crawler $node) {
+                return $node->children()->first()->text();
+            }
+        );
+
+        $this->assertCount(3, $nodeValues);
+        $this->assertEquals('quiz_2', $nodeValues[2]);
+    }
+    
+    public function testQuizNew()
+    {
+        $crawler = $this->client->request('GET', '/quiz/new');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Save')->form();
+
+        $values['quiz']['title'] = 'new';
+        $values['quiz']['_token'] = $form['quiz[_token]']->getValue();
+        $values['quiz']['questions'][0]['text'] = 'text';
+        $values['quiz']['questions'][0]['answers'][0]['text'] = 'answer1';
+        $values['quiz']['questions'][0]['answers'][0]['correct'] = true;
+        $values['quiz']['questions'][0]['answers'][1]['text'] = 'answer2';
+        $values['quiz']['questions'][0]['answers'][1]['correct'] = false;
+
+        $this->client->request($form->getMethod(), $form->getUri(), $values);
+        $quiz = $this->em->getRepository(Quiz::class)->findOneBy(['title' => 'new']);
+
+        $this->assertEquals('new', $quiz->getTitle());
+    }
+
+    public function testQuizEdit()
+    {
+        $crawler = $this->client->request('GET', '/quiz/edit/1');
+        $this->assertEquals(200, $this->client);
+
+        $form = $crawler->selectButton('Save')->form();
+
+        $values['quiz']['title'] = 'edit';
+        $values['quiz']['_token'] = $form['quiz[_token]']->getValue();
+        $values['quiz']['questions'][0]['title'] = 'title';
+        $values['quiz']['questions'][0]['answers'][0]['text'] = 'answer1';
+        $values['quiz']['questions'][0]['answers'][0]['correct'] = true;
+        $values['quiz']['questions'][0]['answers'][1]['text'] = 'answer2';
+        $values['quiz']['questions'][0]['answers'][1]['correct'] = false;
+
+        $this->client->request($form->getMethod(), $form->getUri(), $values);
+        $quiz = $this->em->getRepository(Quiz::class)->findOneBy(['title' => 'edit']);
+
+        $this->assertEquals('edit', $quiz->getTitle());
+    }
+
+    public function testQuizDelete()
+    {
+        $crawler = $this->client->request('GET', '/quiz/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Delete')->form();
+        $values['_token'] = $form['_token']->getValue();
+
+        $this->client->request('DELETE', '/quiz/1', $values);
+        $this->client->followRedirect();
+
+        $quiz = $this->em->getRepository(Quiz::class)->find(1);
+        $this->assertNull($quiz);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+}
